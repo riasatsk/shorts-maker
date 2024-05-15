ba2c9390a1c67b77a847c7eecea9871db4d12c37
Riasat Sk <riasatofficial@outlook.com>
2024-05-15 14:22:05 +0530

    modified:   util/speech.js

--- util/speech.js
+++ util/speech.js
@@ -8,7 +8,7 @@ const speechConfig = sdk.SpeechConfig.fromSubscription(
 
 const audioConfig = sdk.AudioConfig.fromAudioFileOutput(audioFile);
 
-speechConfig.speechSynthesisVoiceName = "hi-IN-KunalNeural";
+speechConfig.speechSynthesisVoiceName = "hi-IN-AaravNeural";
 
 export default function generateAudio(text) {
   let synthesizer = new sdk.SpeechSynthesizer(speechConfig, audioConfig);
